2f31d601cf6c73b0312acd82fffd7ca8f89f417b
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2025-03-10 11:34:57 +0200

    added a link to database

--- beatrunner/firebaseConfig.js
+++ beatrunner/firebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getDatabase } from "firebase/database";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,19 +18,21 @@ const firebaseConfig = {
     storageBucket: "beatr-88a8b.firebasestorage.app",
     messagingSenderId: "522680956492",
     appId: "1:522680956492:web:35f2a3bab5e511cddd66e9",
-    measurementId: "G-0JXC7SP58V"
+    measurementId: "G-0JXC7SP58V",
+    databaseURL: "https://beatr-88a8b-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
-
+const database = getDatabase();
 //npm install - g firebase - tools
 //import { auth } from "./firebaseConfig" Login sivulle tämä
 
 
 
-export { auth };
+export { auth, database };
 
