d3366459b8d0637db43c5ed4bc2b030aa073a32e
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2025-04-27 10:59:24 +0300

    Changed firebaseConfig file to typescript

--- beatrunner/firebaseConfig.ts
+++ beatrunner/firebaseConfig.ts
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
@@ -7,11 +6,6 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBghLChdo2enLZKlifQw__ndtGrbqXRpRw",
     authDomain: "beatr-88a8b.firebaseapp.com",
@@ -23,7 +17,7 @@ const firebaseConfig = {
     databaseURL: "https://beatr-88a8b-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 
@@ -32,10 +26,6 @@ const auth = initializeAuth(app, {
 });
 const database = getDatabase();
 const db = getFirestore();
-//npm install - g firebase - tools
-//import { auth } from "./firebaseConfig" Login sivulle tämä
-
-
 
 export { auth, database, db };
 
