3315ab101a6006b564082df9a92be02c072353f3
Ilona <ilona.hiltunen@myy.haaga-helia.fi>
2025-04-29 00:14:48 +0300

    added .env configs for firebase

--- beatrunner/firebaseConfig.ts
+++ beatrunner/firebaseConfig.ts
@@ -1,28 +1,27 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeAuth } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
-
+import * as firebaseAuth from 'firebase/auth';
+import Constants from 'expo-constants';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBghLChdo2enLZKlifQw__ndtGrbqXRpRw",
-    authDomain: "beatr-88a8b.firebaseapp.com",
-    projectId: "beatr-88a8b",
-    storageBucket: "beatr-88a8b.firebasestorage.app",
-    messagingSenderId: "522680956492",
-    appId: "1:522680956492:web:35f2a3bab5e511cddd66e9",
-    measurementId: "G-0JXC7SP58V",
-    databaseURL: "https://beatr-88a8b-default-rtdb.europe-west1.firebasedatabase.app/"
-};
-
+    apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
+    authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
+    projectId: Constants.expoConfig?.extra?.firebaseProjectId,
+    storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket,
+    messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId,
+    appId: Constants.expoConfig?.extra?.firebaseAppId,
+    measurementId: Constants.expoConfig?.extra?.firebaseMeasurementId,
+  };
 
 const app = initializeApp(firebaseConfig);
 
+const reactNativePersistence = (firebaseAuth as any).getReactNativePersistence;
 
 const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+    persistence: reactNativePersistence(ReactNativeAsyncStorage)
 });
 const database = getDatabase();
 const db = getFirestore();
